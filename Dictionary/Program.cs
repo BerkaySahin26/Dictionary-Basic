c8c63c7b9cde1dfc498978a1d0e11a0969f4a2e6
BerkaySahin26 <berkay.sahin26TR@gmail.com>
2024-08-07 13:53:21 +0300

    Add project files.

---
+++ Dictionary/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Dictionary;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+        myDictionary.Add(12, "A");
+        myDictionary.Add(13, "B");
+        myDictionary.Add(14, "C");
+        myDictionary.Add(15, "D");
+        myDictionary.Write();
+    }
+}
